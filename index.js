5dcad3cdc8386d891df28a423503384bea70c7f2
liudonghua123 <liudonghua123@gmail.com>
2025-03-21 16:17:50 +0800

    update code

--- index.js
+++ index.js
@@ -1,47 +1,53 @@
 // Fill in your client ID and client secret that you obtained
 // while registering the application
-require('dotenv').config()
-const Koa = require('koa');
-const path = require('path');
-const serve = require('koa-static');
-const route = require('koa-route');
-const axios = require('axios');
+require("dotenv").config();
+const Koa = require("koa");
+const path = require("path");
+const serve = require("koa-static");
+const route = require("koa-route");
+const axios = require("axios");
 
-
-const {CLIENT_ID: clientID, CLIENT_SECRET: clientSecret} = process.env;
+const {
+  CLIENT_ID: clientID,
+  CLIENT_SECRET: clientSecret,
+  REDIRECT_URI: redirect_uri,
+  ACCESSTOKEN_URL: accessTokenUrl,
+  PROFILE_URL: profileUrl,
+} = process.env;
 
 const app = new Koa();
 
-const main = serve(path.join(__dirname + '/public'));
+const main = serve(path.join(__dirname + "/public"));
 
-const oauth = async ctx => {
+const oauth = async (ctx) => {
   const code = ctx.request.query.code;
-  const redirect_uri = `http://localhost:8080/oauth/redirect`;
-  console.log('authorization code:', code);
+  console.log("authorization code:", code);
 
-  const url = `https://ids.ynu.edu.cn/authserver/oauth2.0/accessToken?grant_type=authorization_code&client_id=${clientID}&client_secret=${clientSecret}&code=${code}&redirect_uri=${encodeURIComponent(redirect_uri)}`;
-  console.log('try to get access token using url:', url);
+  const url = `${accessTokenUrl}?grant_type=authorization_code&client_id=${clientID}&client_secret=${clientSecret}&code=${code}&redirect_uri=${encodeURIComponent(
+    redirect_uri
+  )}`;
+  console.log("try to get access token using url:", url);
   const tokenResponse = await axios({
-    method: 'post',
+    method: "post",
     url: url,
-    headers: {
-      accept: 'application/json'
-    }
+  }).catch(function (error) {
+    console.log(error.response.status);
+    console.log(error.response.data);
   });
 
   const accessToken = tokenResponse.data.access_token;
   console.log(`access token: ${accessToken}`);
 
-  console.log('try to get user info like mobile phone number');
+  console.log("try to get user info like mobile phone number");
   const result = await axios({
-    method: 'post',
-    url: `https://ids.ynu.edu.cn/authserver/oauthApi/user/profile`,
+    method: "post",
+    url: profileUrl,
     headers: {
-      accept: 'application/json',
+      accept: "application/json",
     },
     data: {
-      access_token: accessToken
-    }
+      access_token: accessToken,
+    },
   });
   console.log(result.data);
   const name = result.data.attributes.nickName;
@@ -50,6 +56,6 @@ const oauth = async ctx => {
 };
 
 app.use(main);
-app.use(route.get('/oauth/redirect', oauth));
+app.use(route.get("/oauth/redirect", oauth));
 
 app.listen(8080);

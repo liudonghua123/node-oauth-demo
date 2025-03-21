32394ec000280f9c9bd78e7327ed4e0488a7ebcc
liudonghua123 <liudonghua123@gmail.com>
2025-03-21 15:07:08 +0800

    update code for ynu

--- index.js
+++ index.js
@@ -1,28 +1,29 @@
 // Fill in your client ID and client secret that you obtained
 // while registering the application
-const clientID = '7e015d8ce32370079895'
-const clientSecret = '2b976af0e6b6ceea2b1554aa31d1fe94ea692cd9'
-
+require('dotenv').config()
 const Koa = require('koa');
 const path = require('path');
 const serve = require('koa-static');
 const route = require('koa-route');
 const axios = require('axios');
 
+
+const {CLIENT_ID: clientID, CLIENT_SECRET: clientSecret} = process.env;
+
 const app = new Koa();
 
 const main = serve(path.join(__dirname + '/public'));
 
 const oauth = async ctx => {
-  const requestToken = ctx.request.query.code;
-  console.log('authorization code:', requestToken);
+  const code = ctx.request.query.code;
+  const redirect_uri = `http://localhost:8080/oauth/redirect`;
+  console.log('authorization code:', code);
 
+  const url = `https://ids.ynu.edu.cn/authserver/oauth2.0/accessToken?grant_type=authorization_code&client_id=${clientID}&client_secret=${clientSecret}&code=${code}&redirect_uri=${encodeURIComponent(redirect_uri)}`;
+  console.log('try to get access token using url:', url);
   const tokenResponse = await axios({
     method: 'post',
-    url: 'https://github.com/login/oauth/access_token?' +
-      `client_id=${clientID}&` +
-      `client_secret=${clientSecret}&` +
-      `code=${requestToken}`,
+    url: url,
     headers: {
       accept: 'application/json'
     }
@@ -31,12 +32,15 @@ const oauth = async ctx => {
   const accessToken = tokenResponse.data.access_token;
   console.log(`access token: ${accessToken}`);
 
+  console.log('try to get user info like mobile phone number');
   const result = await axios({
-    method: 'get',
-    url: `https://api.github.com/user`,
+    method: 'post',
+    url: `https://ids.ynu.edu.cn/authserver/oauthApi/user/getMobile`,
     headers: {
       accept: 'application/json',
-      Authorization: `token ${accessToken}`
+    },
+    data: {
+      access_token: accessToken
     }
   });
   console.log(result.data);
